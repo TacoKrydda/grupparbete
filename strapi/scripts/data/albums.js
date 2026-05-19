1b87b165615ab211c785c8e2cb1e2f1e59b7d121
tacokrydda <jbtand19@gmail.com>
2026-05-19 12:27:30 +0200

    made seeder for strapi and permissions in strapi

---
+++ strapi/scripts/data/albums.js
@@ -0,0 +1,23 @@
+const albums = [
+  {
+    title: "Underbart i all misär",
+    releaseDate: "2021-09-24",
+    artistName: "Miss Li",
+  },
+  {
+    title: "1989",
+    releaseDate: "2014-10-27",
+    artistName: "Taylor Swift",
+  },
+  {
+    title: "Certified Lover Boy",
+    releaseDate: "2021-09-03",
+    artistName: "Drake",
+  },
+  {
+    title: "All the Right Reasons",
+    releaseDate: "2005-10-04",
+    artistName: "Nickelback",
+  },
+];
+export default albums;
